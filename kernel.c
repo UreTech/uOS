a64ccd2f4286fddcfc0446d52bc5818e85242449
UreTech <uretechfdn@gmail.com>
2025-06-03 12:33:08 +0300

    a little bit changed

--- kernel.c
+++ kernel.c
@@ -13,8 +13,8 @@ time_point start, end;
 size_t freeBytes;
 
 void main() {
-    uart_init();
-    uart_print("kernel wake up!\n");
+   // uart_init();
+   // uart_print("kernel wake up!\n");
     delay_ms(1000);
     
     srand(0x330633);
@@ -23,20 +23,10 @@ void main() {
 
     _u_heap_init(); // init heap
     
-    uart_print("creating frame buffer...\n");
+    //uart_print("creating frame buffer...\n");
     mainDisplayBuffer = (Pixel565*)u_malloc(sizeof(Pixel565) * 128 * 128);
     lcd_clear_screen(0x0000);
-    uart_print("created frame buffer!\n");
-
-    uart_print("sd...\n");
-
-    if(sd_init() != 0){
-        uart_print("sd fail\n");
-    }else{
-        uart_print("sd success\n");
-    }
-
-    uart_print("sd end.\n");
+    //uart_print("created frame buffer!\n");
 
     lcd_clear_screen(0x00ff);
    
@@ -44,13 +34,13 @@ void main() {
         start = get_now();
         memset(mainDisplayBuffer, 0x00, sizeof(Pixel565) * 128 * 128);
         lcd_draw_string("uOS v1.0", 0, 0, 0x00f8, mainDisplayBuffer);
-        /*
+        
         freeBytes = _u_free_heap();
         lcd_draw_string(ulltoa(freeBytes), 0, 9, 0xf800, mainDisplayBuffer);
         lcd_draw_string(ulltoa(_u_allocated_block_count()), 0, 18, 0xf800, mainDisplayBuffer);
         lcd_draw_string(ulltoa(elapsedMS), 0, 27, 0xf800, mainDisplayBuffer);
-        */
-        lcd_draw_pixel(100, 100, 0x0000);
+        
+        //lcd_draw_pixel(100, 100, 0x0000);
        // lcd_draw_string(ulltoa(entCount), 0, 9, 0xf800, mainDisplayBuffer);
        // lcd_draw_string(ulltoa(entries[0].sector_count), 0, 18, 0xf800, mainDisplayBuffer);
       //  lcd_draw_string(ulltoa(entries[0].type), 0, 27, 0xf800, mainDisplayBuffer);
--- u_uart.c
+++ u_uart.c
@@ -1,11 +1,13 @@
 #include"u_uart.h"
 #include"u_gpio.h"
+// GPIO
 
 enum {
-    GPFSEL0         = GPIO_BASE + 0x200000,
-    GPSET0          = GPIO_BASE + 0x20001C,
-    GPCLR0          = GPIO_BASE + 0x200028,
-    GPPUPPDN0       = GPIO_BASE + 0x2000E4
+    PERIPHERAL_BASE = 0xFE000000,
+    GPFSEL0         = PERIPHERAL_BASE + 0x200000,
+    GPSET0          = PERIPHERAL_BASE + 0x20001C,
+    GPCLR0          = PERIPHERAL_BASE + 0x200028,
+    GPPUPPDN0       = PERIPHERAL_BASE + 0x2000E4
 };
 
 enum {
@@ -17,10 +19,41 @@ enum {
     Pull_None = 0,
 };
 
+void mmio_write(long reg, unsigned int val) { *(volatile unsigned int *)reg = val; }
+unsigned int mmio_read(long reg) { return *(volatile unsigned int *)reg; }
+
+unsigned int gpio_call(unsigned int pin_number, unsigned int value, unsigned int base, unsigned int field_size, unsigned int field_max) {
+    unsigned int field_mask = (1 << field_size) - 1;
+  
+    if (pin_number > field_max) return 0;
+    if (value > field_mask) return 0; 
+
+    unsigned int num_fields = 32 / field_size;
+    unsigned int reg = base + ((pin_number / num_fields) * 4);
+    unsigned int shift = (pin_number % num_fields) * field_size;
+
+    unsigned int curval = mmio_read(reg);
+    curval &= ~(field_mask << shift);
+    curval |= value << shift;
+    mmio_write(reg, curval);
+
+    return 1;
+}
+
+unsigned int gpio_set     (unsigned int pin_number, unsigned int value) { return gpio_call(pin_number, value, GPSET0, 1, GPIO_MAX_PIN); }
+unsigned int gpio_clear   (unsigned int pin_number, unsigned int value) { return gpio_call(pin_number, value, GPCLR0, 1, GPIO_MAX_PIN); }
+unsigned int gpio_pull    (unsigned int pin_number, unsigned int value) { return gpio_call(pin_number, value, GPPUPPDN0, 2, GPIO_MAX_PIN); }
+unsigned int gpio_function(unsigned int pin_number, unsigned int value) { return gpio_call(pin_number, value, GPFSEL0, 3, GPIO_MAX_PIN); }
+
+void gpio_useAsAlt5(unsigned int pin_number) {
+    gpio_pull(pin_number, Pull_None);
+    gpio_function(pin_number, GPIO_FUNCTION_ALT5);
+}
+
 // UART
 
 enum {
-    AUX_BASE        = GPIO_BASE + 0x215000,
+    AUX_BASE        = PERIPHERAL_BASE + 0x215000,
     AUX_ENABLES     = AUX_BASE + 4,
     AUX_MU_IO_REG   = AUX_BASE + 64,
     AUX_MU_IER_REG  = AUX_BASE + 68,
@@ -57,7 +90,7 @@ void uart_writeByteBlockingActual(unsigned char ch) {
     mmio_write(AUX_MU_IO_REG, (unsigned int)ch);
 }
 
-void uart_print(char *buffer) {
+void uart_writeText(char *buffer) {
     while (*buffer) {
        if (*buffer == '\n') uart_writeByteBlockingActual('\r');
        uart_writeByteBlockingActual(*buffer++);
